5a414f733970ad3aa2d927a0d31d0c7a6b6bc7ab
Thang Cao <shiyoline@gmail.com>
2022-02-04 11:16:10 -0800

    initial backend spring initializr

---
+++ backend/src/test/java/com/sjsuhub/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjsuhub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
