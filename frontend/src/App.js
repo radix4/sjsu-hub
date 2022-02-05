4397aa2e633e52f3e6f89ab7c5b5114254702e37
Thang Cao <shiyoline@gmail.com>
2022-02-04 16:33:51 -0800

    clean up default create-react files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import HomePage from './components/HomePage';
-import RegistrationPage from './components/RegistrationPage';
-import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import HomePage from './components/HomePage'
+import RegistrationPage from './components/RegistrationPage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <Router>
       <div>
-        <Routes >
+        <Routes>
           <Route path='/' component={HomePage} />
           <Route path='/RegistrationPage' component={RegistrationPage} />
-        </Routes >
+        </Routes>
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
