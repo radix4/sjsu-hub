eff26129e870ac072eddfbbe64d529ad09fc6a60
Thang Cao <shiyoline@gmail.com>
2022-02-04 18:09:47 -0800

    add starter code: integrate mysql db

--- backend/src/main/java/com/sjsuhub/Main.java
+++ backend/src/main/java/com/sjsuhub/Main.java
@@ -1,13 +1,13 @@
-package com.sjsuhub.backend;
+package com.sjsuhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
