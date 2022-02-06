0bf8156cb055ecaa9359b0a75be80aeefb678a65
Thang Cao <shiyoline@gmail.com>
2022-02-06 10:42:27 -0800

    Login page starter code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 //import LoginPage from './components/LoginPage'
 import RegistrationPage from './components/RegistrationPage'
 import HomePage from './components/HomePage'
+import LoginPage from './components/LoginPage'
 // import IdeaPage from './components/IdeaPage'
 // import IdeaEditor from './components/IdeaEditor'
 // import Profile from './components/Profile'
@@ -16,10 +17,12 @@ const App = () => {
       <div>
         <Routes>
           {/* On Boot */}
-          <Route path='/' exact element={<HomePage/> } />
+          <Route path='/' exact element={<HomePage />} />
           {/* Other Site */}
           {/* <Route path='/Login' component={LoginPage} /> */}
-          <Route path='/Registration' element ={<RegistrationPage/ >} />
+          <Route path='/Registration' element={<RegistrationPage />} />
+          {/* localhost:3000/Registration */}
+          <Route path='/Login' element={<LoginPage />} />
           {/* <Route path='/IdeaPage/:ideaID' component={IdeaPage} />
           <Route path='/IdeaEditor/:ideaID' component={IdeaEditor} />
           <Route path='/Profile/:ideaID' component={Profile} />
