18a797982ddf7c92f6a9122b41fc928c2b56c819
Joshd510 <joshd510@gmail.com>
2022-02-09 02:26:07 -0800

    homepage cleanup
    cleaned up some formatting

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -2,50 +2,75 @@ import React from 'react';
 import {Container, Col, Form, Row, Button} from 'react-bootstrap';
 
 const HomePage = () => {
+    
 
-    {/* working on flexbox, currently not working so I styled each seperately*/ }
+    const backgroundColor = {
+        'background-color': '#FFF1D7',
+    }
+
+    const title = {
+        'marginLeft': '400px',
+        'marginTop': '300px'
+    }
+
+    const slogan = {
+        'marginLeft' : '410px',
+        'paddingTop' : '50px'
+    }
+
+    const firstRow = {
+        'paddingTop': '50px',
+        'paddingLeft': '400px',
+    }
+
+    const secondRow = {
+        'paddingTop': '50px',
+        'paddingLeft' : '300px',
+        'paddingBottom': '50px',
+    }
+
+    const img = {
+        'paddingRight' : '50px',
+        'border-radius': '50px',
+    }
+
 
 
     return (
 
-        <Container fluid>
+        <Container fluid style={backgroundColor}>
 
-            <Row className="">
-                <Col style= {{marginLeft: '1000px', paddingTop: '10px'}}>
-                <Button variant="primary">Login</Button>
+            <Row className = 'd-flex align-items-center'>
+                <Col sm = {7}>
+                    <Col style = {title}>
+                        <h1> SJSU HUB </h1>
+                    </Col>
+                    <Col style ={slogan}>
+                        <p>A space for all spartans</p>
+                    </Col>
+                    <Col style={firstRow}>
+                        <Button style ={{"margin": "5px"}} variant="outline-primary" size = "lg">Register</Button>
+                        <Button variant="outline-primary" size = "lg">Login</Button>
+                    </Col>
+                    <Col style={secondRow}>
+                        <Button style ={{"margin": "5px"}} variant="outline-primary">Group Chat</Button>
+                        <Button style ={{"margin": "5px"}} variant="outline-primary">Internships and Jobs</Button>
+                        <Button variant="outline-primary">MarketPlace</Button>
+                    </Col>
                 </Col>
-            </Row>
 
-
-            <Row style={{marginTop: '200px', paddingLeft: '150px'}}className = "" >
-                <Col>
-                    <h1>SJSU HUB</h1>
-                </Col>
-                <Col xs={6}>
+                <Col sm = {5}>
                     <img
-                        src='https://blogs.sjsu.edu/newsroom/files/2021/02/strategic-plan-jduarte-031319-19.jpg'
+                        src='https://c1.wallpaperflare.com/preview/500/75/689/san-jose-state-university-california-buildings-campus.jpg'
                         className='img-fluid rounded'
                         alt='example'
-                        style={{ maxWidth: '24rem' }}
+                        style = {img}
                     />
                 </Col>
+                
             </Row>
 
-            <Row style={{marginLeft: '100px'}}>
-                <Col>
-                    <p> A meeting space for all Spartans</p>
-                </Col>
-            </Row>
 
-            <Row style={{marginTop: "100px"}}>
-                <Col sm={3}>
-                    <Button variant="primary">Group Chat</Button> 
-                </Col>
-                <Col sm={3}>
-                    <Button variant="primary">Internships and Jobs</Button>
-                </Col>
-
-            </Row> 
 
         </Container>
     )
