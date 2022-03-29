0f82197621fa9fa51590f4448a1ea79bb999a574
Thang Cao <shiyoline@gmail.com>
2022-03-29 14:25:37 -0700

    Add lombok dependency

--- backend/src/main/java/com/sjsuhub/entities/Post.java
+++ backend/src/main/java/com/sjsuhub/entities/Post.java
@@ -1,10 +1,13 @@
 package com.sjsuhub.entities;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Data
 @Entity
 public class Post {
     @Id
@@ -15,54 +18,5 @@ public class Post {
     private String postContent;
     private String userName; // can prlly just use user? - should be email
     private String postCategory;
-
-    public Integer getId(){
-        return id;
-    }
-
-    public void setId(Integer id){
-        this.id = id;
-    }
-
-    public String getPostContent(){
-        return postContent;
-    }
-
-    public void setPostTitle(String postTitle){
-        this.postTitle = postTitle;
-    }
-
-    public String getPostTitle(){
-        return postTitle;
-    }
-
-    public void setPostContent(String postContent){
-        this.postContent = postContent;
-    }
-
-    public String getUserName(){
-        return userName;
-    }
-
-    public void setUserName(String userName){
-        this.userName = userName;
-    }
-
-    public String getPostCategory(){
-        return postCategory;
-    }
-
-    public void setPostCategory(String postCategory){
-        this.postCategory = postCategory;
-    }
-
-    public String toString(){
-        return  "\n---Post---\n" +
-                "\nID: " + id + " " +
-                "\nContent: " + postContent + 
-                "\nUser Name: " + userName + 
-                "\nPost Category: " + postCategory;
-    }
-    
 
 }
