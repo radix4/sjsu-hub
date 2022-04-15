3755136e982dc4f7d33d4d5e1783cc0dcd954e5d
majlonaa <majlona.aliaj@sjsu.edu>
2022-04-14 18:34:31 -0700

    login page update

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,3 +1,4 @@
+
 import React, { useState } from 'react'
 import { Container, Col, Form, Row, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
@@ -40,7 +41,7 @@ const LoginPage = () => {
 
   const displayAlert = (message, type) => {
     console.log(
-      '\n----\ndisplayAlert Message: ' +
+        '\n----\ndisplayAlert Message: ' +
         message +
         '\nType: ' +
         type +
@@ -55,6 +56,10 @@ const LoginPage = () => {
 
     let form = document.getElementById(`login`)
 
+    //const sanitizeHtml = require('sanitize-html')
+
+    //const email = sanitizeHtml(form.elements.email.value)
+    //const password = sanitizeHtml(form.elements.password.value)
     const email = form.elements.email.value
     const password = form.elements.password.value
 
@@ -86,65 +91,67 @@ const LoginPage = () => {
   }
 
   return (
-    <div>
-      <NavBar />
-      <Container style={backgroundColor} fluid className='mt-5'>
-        <Row>
-          {/* ================ LEFT COLUMN =================== */}
-          <Col lg={4} md={5} sm={12} className='text-center mt-4 p-3'>
-            <Form.Group style={controlMargin}>
-              <Col>
-                <h3>
-                  <b>Welcome Back :)</b>
-                </h3>
-              </Col>
-            </Form.Group>
-            <img style={iconimg} src={user} alt='icon' />
-            <br />
-            <Form id='login' onSubmit={handleLogin}>
-              <Form.Group style={controlMargin} controlId='email'>
-                <Form.Control
-                  type='text'
-                  style={roundedCorners}
-                  placeholder='SJSU Email'
-                />
-              </Form.Group>
-              <Form.Group style={controlMargin} controlId='password'>
-                <Form.Control
-                  type='password'
-                  style={roundedCorners}
-                  placeholder='Password'
-                />
-              </Form.Group>
-              <Notification message={errorMessage} type={typeAlert} />
-              <Form.Group style={controlMargin}>
-                <Button type='submit' style={submitButton}>
-                  <b>Login</b>
-                </Button>
-              </Form.Group>
-              <br /> <br />
+      <div>
+        <NavBar />
+        <Container style={backgroundColor} fluid className='mt-5'>
+          <Row>
+            {/* ================ LEFT COLUMN =================== */}
+            <Col lg={4} md={5} sm={12} className='text-center mt-4 p-3'>
               <Form.Group style={controlMargin}>
                 <Col>
-                  <h6>
-                    <b>Don't have an account yet?</b>
-                  </h6>
-                  <Link to='/Registration'>
-                    <Button style={submitButton}>
-                      <b>Sign Up</b>
-                    </Button>
-                  </Link>
+                  <h3>
+                    <b>Welcome Back :)</b>
+                  </h3>
                 </Col>
               </Form.Group>
-            </Form>
-          </Col>
+              <img style={iconimg} src={user} alt='icon' />
+              <br />
+              <Form id='login' onSubmit={handleLogin}>
+                <Form.Group style={controlMargin} controlId='email'>
+                  <Form.Control
+                      type='text'
+                      style={roundedCorners}
+                      placeholder='SJSU Email'
+                      required
+                  />
+                </Form.Group>
+                <Form.Group style={controlMargin} controlId='password'>
+                  <Form.Control
+                      type='password'
+                      style={roundedCorners}
+                      placeholder='Password'
+                      required
+                  />
+                </Form.Group>
+                <Notification message={errorMessage} type={typeAlert} />
+                <Form.Group style={controlMargin}>
+                  <Button type='submit' style={submitButton}>
+                    <b>Login</b>
+                  </Button>
+                </Form.Group>
+                <br /> <br />
+                <Form.Group style={controlMargin}>
+                  <Col>
+                    <h6>
+                      <b>Don't have an account yet?</b>
+                    </h6>
+                    <Link to='/Registration'>
+                      <Button style={submitButton}>
+                        <b>Sign Up</b>
+                      </Button>
+                    </Link>
+                  </Col>
+                </Form.Group>
+              </Form>
+            </Col>
 
-          {/* ================ RIGHT COLUMN =================== */}
-          <Col lg={6} md={3} sm={12} className='text-center'>
-            <img className='w-100' src={login} alt='' />
-          </Col>
-        </Row>
-      </Container>
-    </div>
+            {/* ================ RIGHT COLUMN =================== */}
+            <Col lg={6} md={3} sm={12} className='text-center'>
+              <img className='w-100' src={login} alt='' />
+            </Col>
+          </Row>
+        </Container>
+      </div>
   )
 }
 
--- frontend/src/components/RegistrationPage.js
+++ frontend/src/components/RegistrationPage.js
@@ -4,6 +4,7 @@ import userService from '../services/users'
 import Notification from './Notification'
 import { Link } from 'react-router-dom'
 import NavBar from './NavBar'
+//import sanitizeHtml from "sanitize-html";
 
 const RegistrationPage = () => {
   const [errorMessage, setErrorMessage] = useState(null)
@@ -42,7 +43,7 @@ const RegistrationPage = () => {
 
   const displayAlert = (message, type) => {
     console.log(
-      '\n----\ndisplayAlert Message: ' +
+        '\n----\ndisplayAlert Message: ' +
         message +
         '\nType: ' +
         type +
@@ -57,16 +58,56 @@ const RegistrationPage = () => {
 
     var form = document.getElementById('registration')
 
+    /* const sanitizeHtml = require('sanitize-html')
+
+     const firstName = sanitizeHtml(form.elements.firstName.value)
+     const lastName = sanitizeHtml(form.elements.lastName.value)
+     const email = sanitizeHtml(form.elements.sjsuEmail.value)
+     const password = sanitizeHtml(form.elements.password.value)
+     const confirm = sanitizeHtml(form.elements.confirm.value)
+ */
+
     const firstName = form.elements.firstName.value
     const lastName = form.elements.lastName.value
     const email = form.elements.sjsuEmail.value
     const password = form.elements.password.value
     const confirm = form.elements.confirm.value
 
+    if (firstName == ""){
+      console.log('Error: First Name is empty')
+      displayAlert('First Name must be filled out.', 'error')
+      return
+    }
+
+    if (lastName == ""){
+      console.log('Error: Last Name is empty')
+      displayAlert('Last Name must be filled out.', 'error')
+      return
+    }
+
+    if (email == ""){
+      console.log('Error: Email is empty')
+      displayAlert('Email must be filled out.', 'error')
+      return
+    }
+
+    if (password == ""){
+      console.log('Error: Password is empty')
+      displayAlert('Password must be filled out.', 'error')
+      return
+    }
+    if (confirm == ""){
+      console.log('Error: Confirmation password is empty')
+      displayAlert('Confirmation password must be filled out.', 'error')
+      return
+    }
+
+
     if (!email.includes('@sjsu.edu')) {
       displayAlert('Email must be SJSU student email.', 'error')
       return
     }
+
     if (password !== confirm) {
       console.log('Error: Passwords do not match')
       displayAlert('Passwords do not match. Please try again.', 'error')
@@ -95,104 +136,117 @@ const RegistrationPage = () => {
   }
 
   return (
-    <div>
-      <NavBar />
-      <Container style={backgroundColor} fluid>
-        <Row className='align-items-center d-flex flex-wrap-reverse'>
-          {/* ================ LEFT COLUMN =================== */}
-          <Col style={leftColStyle} md={6}>
-            <h2>Sign up</h2>
-            <br />
-            <Form id='registration' onSubmit={addUser}>
-              <Form.Group style={controlMargin} controlId='firstName'>
-                <Form.Label>First Name</Form.Label>
-                <Col md={8}>
-                  <Form.Control
-                    type='text'
-                    style={roundedCorners}
-                    placeholder='First Name'
-                  />
-                </Col>
-              </Form.Group>
+      <div>
+        <NavBar />
+        <Container style={backgroundColor} fluid>
+          <Row className='align-items-center d-flex flex-wrap-reverse'>
+            {/* ================ LEFT COLUMN =================== */}
+            <Col style={leftColStyle} md={6}>
+              <h2>Sign up</h2>
+              <br />
+              <Form id='registration' onSubmit={addUser}>
+                <Form.Group style={controlMargin} controlId='firstName'>
+                  <Form.Label>First Name</Form.Label>
+                  <Col md={8}>
+                    <Form.Control
+                        type='text'
+                        style={roundedCorners}
+                        placeholder='First Name'
+                        required
+                        maxlength= "100"
+                    />
+                  </Col>
+                </Form.Group>
 
-              <Form.Group style={controlMargin} controlId='lastName'>
-                <Form.Label>Last Name</Form.Label>
-                <Col md={8}>
-                  <Form.Control
-                    type='text'
-                    style={roundedCorners}
-                    placeholder='Last Name'
-                  />
-                </Col>
-              </Form.Group>
+                <Form.Group style={controlMargin} controlId='lastName'>
+                  <Form.Label>Last Name</Form.Label>
+                  <Col md={8}>
+                    <Form.Control
+                        type='text'
+                        style={roundedCorners}
+                        placeholder='Last Name'
+                        required
+                        maxlength = "100"
+                    />
+                  </Col>
+                </Form.Group>
 
-              <Form.Group style={controlMargin} controlId='sjsuEmail'>
-                <Form.Label>SJSU email address</Form.Label>
-                <Col md={8}>
-                  <Form.Control
-                    type='text'
-                    style={roundedCorners}
-                    placeholder='Email'
-                  />
-                </Col>
-              </Form.Group>
+                <Form.Group style={controlMargin} controlId='sjsuEmail'>
+                  <Form.Label>SJSU email address</Form.Label>
+                  <Col md={8}>
+                    <Form.Control
+                        type='text'
+                        style={roundedCorners}
+                        placeholder='Email'
+                        title = "Enter a valid SJSU email address: xxx@sjsu.edu"
+                        pattern ="[a-zA-Z0-9]+@sjsu.edu"
+                        required
+                    />
+                  </Col>
+                </Form.Group>
 
-              <Form.Group style={controlMargin} controlId='password'>
-                <Form.Label>Password</Form.Label>
-                <Col md={8}>
-                  <Form.Control
-                    type='password'
-                    style={roundedCorners}
-                    placeholder='Password'
-                  />
-                </Col>
-              </Form.Group>
+                <Form.Group style={controlMargin} controlId='password'>
+                  <Form.Label>Password</Form.Label>
+                  <Col md={8}>
+                    <Form.Control
+                        type='password'
+                        style={roundedCorners}
+                        placeholder='Password'
+                        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                        title="Password must contain a lowercase letter, an uppercase letter, a number, and a minimum of 8 characters"
+                        required
+                        maxlength="20"
+                        minlength="8"
+                    />
+                  </Col>
+                </Form.Group>
 
-              <Form.Group style={controlMargin} controlId='confirm'>
-                <Form.Label>Confirm password</Form.Label>
-                <Col md={8}>
-                  <Form.Control
-                    type='password'
-                    style={roundedCorners}
-                    placeholder='Confirm Password'
-                  />
-                </Col>
-              </Form.Group>
-              <Notification message={errorMessage} type={typeAlert} />
-              <Form.Group as={Row}>
-                <Col md={{ span: 10, offset: 4 }}>
-                  <Button type='submit' style={submitButton}>
-                    <b>Create Account</b>
-                  </Button>
-                </Col>
-              </Form.Group>
-            </Form>
-          </Col>
-
-          {/* ================ RIGHT COLUMN =================== */}
-          <Col style={rightColStyle} md={6}>
-            <Row style={centering}>
-              <h2>
-                <b>Already have an account?</b>
-              </h2>
-              <hr class='rounded'></hr>
-              <hr class='rounded'></hr>
-              <hr class='rounded'></hr>
-
-              <Form.Group as={Row}>
-                <Col>
-                  <Link to='/Login'>
-                    <Button style={submitButton}>
-                      <b>Login</b>
+                <Form.Group style={controlMargin} controlId='confirm'>
+                  <Form.Label>Confirm password</Form.Label>
+                  <Col md={8}>
+                    <Form.Control
+                        type='password'
+                        style={roundedCorners}
+                        placeholder='Confirm Password'
+                        required
+                    />
+                  </Col>
+                </Form.Group>
+                <Notification message={errorMessage} type={typeAlert} />
+                <Form.Group as={Row}>
+                  <Col md={{ span: 10, offset: 4 }}>
+                    <Button type='submit' style={submitButton}>
+                      <b>Create Account</b>
                     </Button>
-                  </Link>
-                </Col>
-              </Form.Group>
-            </Row>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+                  </Col>
+                </Form.Group>
+              </Form>
+            </Col>
+
+            {/* ================ RIGHT COLUMN =================== */}
+            <Col style={rightColStyle} md={6}>
+              <Row style={centering}>
+                <h2>
+                  <b>Already have an account?</b>
+                </h2>
+                <hr class='rounded'></hr>
+                <hr class='rounded'></hr>
+                <hr class='rounded'></hr>
+
+                <Form.Group as={Row}>
+                  <Col>
+                    <Link to='/Login'>
+                      <Button style={submitButton}>
+                        <b>Login</b>
+                      </Button>
+                    </Link>
+                  </Col>
+                </Form.Group>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
   )
 }
 
