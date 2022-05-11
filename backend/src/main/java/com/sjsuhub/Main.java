75156591d75a8fe9a965eb66f222dc081c4e03dc
Thang Cao <shiyoline@gmail.com>
2022-05-11 09:03:13 -0700

    Update tutoring sessions with data

--- backend/src/main/java/com/sjsuhub/Main.java
+++ backend/src/main/java/com/sjsuhub/Main.java
@@ -3,6 +3,7 @@ package com.sjsuhub;
 import com.sjsuhub.controllers.UserController;
 import com.sjsuhub.entities.TutoringSession;
 import com.sjsuhub.entities.User;
+import com.sjsuhub.repositories.EventRepository;
 import com.sjsuhub.repositories.TutoringSessionRepository;
 import com.sjsuhub.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,9 @@ public class Main implements CommandLineRunner {
 	@Autowired
 	private TutoringSessionRepository tutoringSessionRepository;
 
+	@Autowired
+	private EventRepository eventRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
@@ -36,6 +40,7 @@ public class Main implements CommandLineRunner {
 		/* Clean up database data */
 		userRepository.deleteAll();
 		tutoringSessionRepository.deleteAll();
+		eventRepository.deleteAll();
 
 
 		/* User table: Pre-configure data */
@@ -58,13 +63,15 @@ public class Main implements CommandLineRunner {
 
 		/* Tutoring Session table: Pre-configure data */
 		TutoringSession tutoringSession =
-				new TutoringSession( "test1", "test1", "test1", "test1", "test1", "test1", "test1", "test", true);
+				new TutoringSession( "Josh", "josh@sjsu.edu", "Expert in DS&Algo, a senior at SJSU", "(925) 123-1234", "Available for tutoring", "Data Structure and Algorithm", "Mon-Weds | 3 to 4 PM", "Online via Zoom", true);
 		TutoringSession tutoringSession2 =
-				new TutoringSession( "test2", "test2", "test2", "test2", "test2", "test2", "test2", "test2", false);
+				new TutoringSession( "Rhea", "Rhea@sjsu.edu", "A fabulous Spartan student. Great at CS", "(408) 333 4444", "Tutee needed! Please contact for more info.", "Software Management", "Fri-Sat | 9 to 5 PM", "On-campus SJSU Library", true);
+		TutoringSession tutoringSession3 =
+				new TutoringSession( "Thang", "thang@sjsu.edu", "A struggling SWE student at SJSU, need tutor!", "(408) 333 4444", "Help needed for CS 101", "Intro to Computer Science", "Fri-Sat | 9 to 5 PM", "Online via Zoom", false);
 
 		tutoringSessionRepository.save(tutoringSession);
 		tutoringSessionRepository.save(tutoringSession2);
-
+		tutoringSessionRepository.save(tutoringSession3);
 
 
 
